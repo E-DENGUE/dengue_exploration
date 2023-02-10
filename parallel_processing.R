090ae5f616a6115dac737cb37ce977dc9d4a416b
Elisabethnelson <elisabeth.nelson@yale.edu>
2023-02-09 19:24:31 -0500

    update

---
+++ parallel_processing.R
@@ -0,0 +1,29 @@
+library(parallel) 
+library(future)
+
+source('base_model_cluster.R')
+
+n_cores <- max(availableCores(methods=c("system")) - 1, 1)
+
+cl1 <- parallel::makeCluster(n_cores)
+
+
+clusterEvalQ(cl=cl1, {
+  library(lubridate, quietly = TRUE)
+  library(dplyr, quietly = TRUE)
+  library(CARBayes)
+  library(CARBayesST)
+  library(TSDT)
+})
+
+clusterExport(cl1, c('mod.func','advance_missing_func', 'base_model_final', 
+                     'neighb.mat_2', 'samples_to_quant'), environment())
+
+mod1 <- parLapply(
+  cl = cl1,
+  1:73,
+  fun = mod.func
+)
+
+stopCluster(cl1)
+
