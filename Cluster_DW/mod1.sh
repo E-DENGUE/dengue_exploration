23c14fbe896aba6fdac33215871c3a834c2b2975
DanWeinberger <56560444+DanWeinberger@users.noreply.github.com>
2023-09-24 22:09:10 -0400

    Update mod1.sh to allow full parallelization with modulo
    allow

--- Cluster_DW/mod1.sh
+++ Cluster_DW/mod1.sh
@@ -4,21 +4,20 @@
 #SBATCH --partition day,scavenge
 #SBATCH --mail-user=daniel.weinberger@yale.edu
 #SBATCH --cpus-per-task=8
-#SBATCH --array=1-108   # Specify the total number of tasks
+#SBATCH --array=1-1080   # If k models and J hold out time points this is 1- j*k
 
 # Define the mJ and K codes
 module load R/4.2.0-foss-2020b
 
-# Calculate the values of J and K for this task
-START_VALUE_J=1
-END_VALUE_J=108
-START_VALUE_K=1
-END_VALUE_K=10
+# J:1-108 time periods
+# K 1:10 models
 
-# Calculate the task-specific values of J and K
+# Use modulos to iterate through all task IDs
 task_id=$SLURM_ARRAY_TASK_ID
-j=$(( (task_id - 1) % (END_VALUE_J - START_VALUE_J + 1) + START_VALUE_J ))
-k=$(( (task_id - 1) / (END_VALUE_J - START_VALUE_J + 1) + START_VALUE_K ))
+j=$(( task_id  / 10 )) # $(( )) does arithmetic evaluation; Bash performs integer division so floor() is default
+k=$(( task_id  % 10  + 1 )) # $(( )) does arithmetic evaluation
 
-# Run your R script with the task-specific J and K
+    # Run your R script with the task-specific J and K
 Rscript mod1.R "$j" "$k"
+
+# done
