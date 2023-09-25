154df457dac81ee72bebf4ee492159fb5fefc6c5
DanWeinberger <56560444+DanWeinberger@users.noreply.github.com>
2023-09-25 09:17:22 -0400

    explicitly defined N models in .sh

--- Cluster_DW/mod1.sh
+++ Cluster_DW/mod1.sh
@@ -6,16 +6,19 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --array=1-1080   # If k models and J hold out time points this is 1- j*k
 
-# Define the mJ and K codes
+#Define the number of models being tested
+N_models=10
+
+#Load R
 module load R/4.2.0-foss-2020b
 
 # J:1-108 time periods
-# K 1:10 models
+# K 1:N_models models
 
 # Use modulos to iterate through all task IDs
 task_id=$SLURM_ARRAY_TASK_ID
-j=$(( task_id  / 10 )) # $(( )) does arithmetic evaluation; Bash performs integer division so floor() is default
-k=$(( task_id  % 10  + 1 )) # $(( )) does arithmetic evaluation
+j=$(( task_id  / N_models )) # $(( )) does arithmetic evaluation; Bash performs integer division so floor() is default
+k=$(( task_id  % N_models  + 1 )) # $(( )) does arithmetic evaluation
 
     # Run your R script with the task-specific J and K
 Rscript mod1.R "$j" "$k"
